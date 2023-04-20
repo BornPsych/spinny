1d9eca74c47476bd0eb5f52c6a1180f3f5a19286
BornPsych <yogeshshahi120@gmail.com>
2023-04-20 15:13:51 +0530

    commit 6

--- test/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import user1, action
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = user1
-        read_only_fields = ['id',]
-        fields = '__all__'
--- test/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:pk>',views.UserView.as_view()),
-    path('create/user', views.createUserView.as_view()),
-    # path('<int:pk>', views.model_view)
-]
